2e2b643ecabc88d4beb887b1897053384967dc37
yusuf-inci <yusufinci@protonmail.com>
2024-01-23 09:58:34 +0300

    updated bucket & cluster name

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,13 +27,11 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "gitopsterrastate"
+    bucket = "vprofile55actions"
     key    = "terraform.tfstate"
     region = "us-east-2"
   }
 
   required_version = "~> 1.6.3"
 }
-##
-##
-##
+
